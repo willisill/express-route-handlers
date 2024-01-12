98301655a65545959559bd0248845ce0871b6df7
William <byweimar@gmail.com>
2024-01-11 20:58:21 -0500

    added changes

--- server/app.js
+++ server/app.js
@@ -44,6 +44,32 @@ app.get('/artists/:artistId', (req, res) => {
   res.json(getAlbumsByArtistId(artistId))
 })
 
+//Edit artist by ID
+
+app.put('/artists/:artistId', (req, res) => {
+  const artistId = req.params.artistId;
+  const editedData = req.body;
+  res.json(editArtistByArtistId(artistId, editedData));
+})
+
+//Delete artist by ID
+
+app.delete('artists/:artistId', (req, res) => {
+  const artistId = req.params.artistId
+  req.body = { message: "successfully deleted" }
+  res.send(req.body)
+  res.json(deleteArtistByArtistId(artistId))
+})
+
+//All albums of specific artist by ID
+
+app.get('/artists/:artistId/albums', (req, res) => {
+  const { artistId } = req.params
+  res.json(getAlbumsByArtistId(artistId))
+})
+
+
+
 // DO NOT MODIFY
 if (require.main === module) {
   const port = 8000;
